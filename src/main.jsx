0badf3b908ce2354994da56a8adff7ec5240a6da
Vincent Larcher <vincent@tymate.com>
2021-11-21 17:57:36 +0100

    feat: update architecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { MargaretProvider } from "@tymate/margaret";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MargaretProvider>
+      <App />
+    </MargaretProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
